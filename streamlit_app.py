82d64d8a96a9a0cecea03cb1d08853bc13d2001d
Lakshmi Tammana <lakshmi.tammana@invesco.com>
2023-01-30 21:17:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,12 @@ streamlit.title("Training")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
+
