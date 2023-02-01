aabe6f784665f79bce297ee0e8c481f9e2c862f3
Lakshmi Tammana <lakshmi.tammana@invesco.com>
2023-02-01 16:54:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("Training")
 streamlit.header('Breakfast Menu')
@@ -15,3 +16,5 @@ fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.ind
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
