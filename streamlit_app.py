e3e7644ffaf550c15aa422859324caf3b776db21
Lakshmi Tammana <lakshmi.tammana@invesco.com>
2023-02-01 22:57:00 -0600

    connecting to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+mycur = my_cnx.cursor()
+my_cur.execute("select current_user(),current_account(),current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+                                     
